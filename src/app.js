c508f9edb8acc72bdf1129ac2d85cd2f2940be50
Silvia Stoyanova <stoianova.silvia@yahoo.com>
2020-01-15 12:24:51 +0200

    add CRUD operations

--- src/app.js
+++ src/app.js
@@ -1,28 +1,55 @@
 $(document).ready(function () {
-    $("#grid").kendoGrid({
-        columns: [
-            {
-                selectable: true, 
-                width: "60px"
+    var dataSource = new kendo.data.DataSource({
+            pageSize: 10,
+            transport: {
+                read:  {
+                    url: "https://demos.telerik.com/kendo-ui/service/Products",
+                    dataType: "jsonp"
+                },
+                update: {
+                    url: "https://demos.telerik.com/kendo-ui/service/Products/Update",
+                    dataType: "jsonp"
+                },
+                destroy: {
+                    url: "https://demos.telerik.com/kendo-ui/service/Products/Destroy",
+                    dataType: "jsonp"
+                },
+                create: {
+                    url: "https://demos.telerik.com/kendo-ui/service/Products/Create",
+                    dataType: "jsonp"
+                },
+                parameterMap: function (options, operation) {
+                    if (operation !== "read" && options.models) {
+                        return { models: kendo.stringify(options.models) };
+                    }
+                }
             },
-            {
-                field: "name",
-                title: "Contact Name" 
-            },
-            {
-                field: "phone",
-                title: "Phone Number"
-            },
-            {
-                field: "email",
-                title: "E-mail"
+            schema: {
+                model: {
+                    id: "ProductID",
+                    fields: {
+                        ProductID: { editable: false, nullable: true },
+                        ProductName: { validation: { required: true } },
+                        UnitPrice: { type: "number", validation: { required: true, min: 1 } },
+                        UnitsInStock: { type: "number", validation: { min: 0, required: true } }
+                    }
+                }
             }
-        ],
-        pageable: {
-            pageSizes: [10, 25, 100],
-        },
-        persistSelection: true,
-        scrollable:false,
-        sortable: true,
     });
- });
+
+    $("#grid").kendoGrid({
+        dataSource: dataSource,
+        pageable: {
+            buttonCount: 5,
+            pageSizes: [10, 25, 100]
+        },
+        scrollable: false,
+        persistSelection: true,
+        columns: [
+            { selectable: true, width: "50px" },
+            { field:"ProductName", title: "Product Name" },
+            { field: "UnitPrice", title:"Unit Price", format: "{0:c}"},
+            { field: "UnitsInStock", title:"Units In Stock"},
+            ]
+    });
+});
