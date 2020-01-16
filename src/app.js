40d3299bc55e4a63381265dab9c5d055a3e2bf12
Silvia Stoyanova <stoianova.silvia@yahoo.com>
2020-01-16 21:00:04 +0200

    add optional code for buttons outside the grid

--- src/app.js
+++ src/app.js
@@ -1,41 +1,41 @@
 $(document).ready(function () {
     const dataSource = new kendo.data.DataSource({
-            pageSize: 10,
-            transport: {
-                read:  {
-                    url: "https://demos.telerik.com/kendo-ui/service/Products",
-                    dataType: "jsonp"
-                },
-                update: {
-                    url: "https://demos.telerik.com/kendo-ui/service/Products/Update",
-                    dataType: "jsonp"
-                },
-                destroy: {
-                    url: "https://demos.telerik.com/kendo-ui/service/Products/Destroy",
-                    dataType: "jsonp"
-                },
-                create: {
-                    url: "https://demos.telerik.com/kendo-ui/service/Products/Create",
-                    dataType: "jsonp"
-                },
-                parameterMap: function (options, operation) {
-                    if (operation !== "read" && options.models) {
-                        return { models: kendo.stringify(options.models) };
-                    }
-                }
+        pageSize: 10,
+        transport: {
+            read: {
+                url: "https://demos.telerik.com/kendo-ui/service/Products",
+                dataType: "jsonp"
             },
-            batch: true,
-            schema: {
-                model: {
-                    id: "ProductID",
-                    fields: {
-                        ProductID: { editable: false, nullable: true },
-                        ProductName: { validation: { required: true } },
-                        UnitPrice: { type: "number", validation: { required: true, min: 1 } },
-                        UnitsInStock: { type: "number", validation: { min: 0, required: true } }
-                    }
+            update: {
+                url: "https://demos.telerik.com/kendo-ui/service/Products/Update",
+                dataType: "jsonp"
+            },
+            destroy: {
+                url: "https://demos.telerik.com/kendo-ui/service/Products/Destroy",
+                dataType: "jsonp"
+            },
+            create: {
+                url: "https://demos.telerik.com/kendo-ui/service/Products/Create",
+                dataType: "jsonp"
+            },
+            parameterMap: function (options, operation) {
+                if (operation !== "read" && options.models) {
+                    return { models: kendo.stringify(options.models) };
                 }
             }
+        },
+        batch: true,
+        schema: {
+            model: {
+                id: "ProductID",
+                fields: {
+                    ProductID: { editable: false, nullable: true },
+                    ProductName: { validation: { required: true } },
+                    UnitPrice: { type: "number", validation: { required: true, min: 1 } },
+                    UnitsInStock: { type: "number", validation: { min: 0, required: true } }
+                }
+            }
+        }
     });
 
     const toolbar = kendo.template($("#custom-toolbar").html());
@@ -51,20 +51,34 @@ $(document).ready(function () {
         persistSelection: true,
         columns: [
             { selectable: true, width: "50px" },
-            { field:"ProductName", title: "Product Name" },
-            { field: "UnitPrice", title:"Unit Price", format: "{0:c}"},
-            { field: "UnitsInStock", title:"Units In Stock"},
+            { field: "ProductName", title: "Product Name" },
+            { field: "UnitPrice", title: "Unit Price", format: "{0:c}" },
+            { field: "UnitsInStock", title: "Units In Stock" },
         ],
         editable: "inline"
     });
 
     const grid = $("#grid").data("kendoGrid");
 
-        $("#addItem").on("click", function() {
-            grid.addRow();
-        });
+    $("#addItem").on("click", function() {
+        grid.addRow();
+    });
 
-        $("#saveItem").on("click", function() {
-            grid.saveRow();
-        });
+    $("#saveItem").on("click", function () {
+        grid.saveRow();
+    });
+
+// *** Please uncomment the code below and in index.html for buttons outside the grid toolbar ***
+
+//     $(".addItem").kendoButton();
+//     const addItemButton = $(".addItem").data("kendoButton");
+//     addItemButton.bind("click", function () {
+//         grid.addRow();
+//     });
+
+//     $(".saveItem").kendoButton();
+//     const saveItemButton = $(".saveItem").data("kendoButton");
+//     saveItemButton.bind("click", function () {
+//         grid.saveRow();
+//     });
 });
